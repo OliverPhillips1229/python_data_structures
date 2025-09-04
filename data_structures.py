deec8a4cc60f813be3f8eeeb68d3db9b724dcf50
Oliver Levine <oliversfphillips@icloud.com>
2025-09-04 07:37:14 -0700

    PDS page 5

--- data_structures.py
+++ data_structures.py
@@ -235,3 +235,46 @@ print(chips_set)
 # prints: {'potato', 'paint', 'computer'}
 # remember, sets are not ordered - your elements may print in a different order
 
+# Say we needed to square all of the numbers in a list and put them into a new list, 
+# we might use a for loop like this:
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+squares = []
+# we want 'n * n' for each 'n' in nums 
+for n in nums:
+    squares.append(n * n)
+print(squares)
+# prints [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# A list comprehension can reduce this code:
+squares = []
+for n in nums:
+    squares.append(n * n)
+    # To this:
+    squares = [n * n for n in nums]
+    
+# Here’s the basic syntax of a list comprehension:
+# [<expression> for <item> in <list>]
+# This reads as: I want <expression> for each <item> in <list>
+
+# We just saw how list comprehensions are an excellent way to map a 
+# list, but they can be used for filtering too.
+# Again, let’s start with a non-comprehension approach by using a for 
+# in loop to map and filter nums:
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+even_squares = []
+# we want 'n * n' for each 'n' in nums  if 'n * n' is even
+for n in nums:
+    square = n * n 
+    if square % 2 == 0:
+        even_squares.append(square)
+print(even_squares)
+# prints: [4, 16, 36, 64, 100]
+
+# Again, list comprehensions reduce the above from:
+even_squares = []
+for n in nums:
+    square = n * n 
+    if square % 2 == 0:
+        even_squares.append(square)
+# To this one-liner:
+even_squares = [n * n for n in nums if (n * n) % 2 == 0]
